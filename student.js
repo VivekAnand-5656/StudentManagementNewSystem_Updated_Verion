8e81c5aa9ae4fa0f2db5b74d0640f48ed8eb5156
Vivek Anand <va691187@gmail.com>
2026-01-08 00:13:36 +0530

    Form Validation Complete

---
+++ student.js
@@ -0,0 +1,40 @@
+const form = document.getElementById("formSubmit");
+
+const students = JSON.parse(localStorage.getItem("students")) || [];
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const studentName = document.getElementById("name").value.trim();
+    const email = document.getElementById("email").value.trim();
+    const mobile = document.getElementById("mobile").value.trim();
+    const dob = document.getElementById("date").value;
+    const course = document.getElementById("course").value;
+    const year = document.getElementById("year").value;
+    const address = document.getElementById("address").value.trim();
+    const enroll = document.getElementById("enroll").value.trim();
+
+    if(mobile.length != 10){ 
+        alert("Enter Valid Mobile Number")
+        return;
+    } else if(year.length != 4){
+         alert("Enter Valid Year")
+        return;
+    }
+    // ----------------------- Bs ab Table m Add Krna hai  ----------
+
+    const student = {
+        stuName: studentName,
+        mail: email,
+        courses: course,
+        years: year,
+        add: address,
+        enrollment: enroll,
+        mob:mobile,
+        dobm:dob
+    }
+    students.push(student);
+    localStorage.setItem("students", JSON.stringify(students));
+    alert("Student Added Successfully");
+    form.reset();
+
+});
